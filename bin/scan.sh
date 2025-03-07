8692d4c3f15c6e0b7fc29cab6773f4b31a690f28
kyuff <soren@sxl.dk>
2025-03-07 18:48:04 +0100

    Add bin/scan.sh file

---
+++ bin/scan.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "Running scan..."
+result=$(/scan | base64 -w 0)
+echo "RESULT=${result}" >> $GITHUB_OUTPUT
+echo "---------------------------------"
+echo "Found dependencies:"
+echo $result | base64 -d
+echo "---------------------------------"
+echo "Displaying file system:"
+find . -type f
